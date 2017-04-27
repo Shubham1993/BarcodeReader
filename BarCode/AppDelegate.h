13fd7b980caea6260f75980811c36ee92e57b3d6
Shubham <shubham@shubhams-mac.local>
2017-04-27 23:09:05 +0530

    Initial Commit

---
+++ BarCode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BarCode
+//
+//  Created by Shubham on 27/04/17.
+//  Copyright © 2017 Shubham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BarCode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BarCode
+//
+//  Created by Shubham on 27/04/17.
+//  Copyright © 2017 Shubham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
